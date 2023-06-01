02326b2ac080766ff2275dd44262de4af8ef1379
DESKTOP-Q5AQMJF\김현찬 <holiday@soongsil.ac.kr>
2023-06-01 19:22:13 +0900

    Ui 레이아웃 변경

--- SchoolManager.py
+++ SchoolManager.py
@@ -22,91 +22,79 @@ class SchoolManager():
         self.create_details_button_frame()
         
     def create_main_frame(self):
-
-        self.main_frame = customtkinter.CTkFrame(self.root,bg_color="#222222",fg_color="#222222",border_color="#222222")
-        self.main_frame.pack()
-
+        # Variables
         column, row = 0, 0
         columnCnt = 3
 
-        for school in self.schools:
-            schoolCnt = column + row * columnCnt
+        # Create frame
+        self.main_frame = customtkinter.CTkFrame(self.root,bg_color="#222222",fg_color="#222222",border_color="#222222")
+        self.main_frame.pack()
 
+        # Create buttons with logos for each school
+        for school in self.schools:
+            # Variables
+            schoolCnt = column + row * columnCnt
             logoPath = school.logoPath
             name = school.name
 
+            # Create and configure logo
             logoImage = Image.open(self.basePath + logoPath)
             logoImage = logoImage.resize((259, 250), Image.ANTIALIAS)
-
             logo = ImageTk.PhotoImage(logoImage)
 
+            # Create and pack canvas
             canvas = customtkinter.CTkFrame(self.main_frame)
             canvas.grid(column=column, row=row)
 
+            # Create and pack button
             button = customtkinter.CTkButton(canvas, command=partial(self.read, schoolCnt), image=logo,text="",fg_color="#222222")
             button.pack()
 
+            # Create and pack label
             customtkinter.CTkLabel(canvas, text=name).pack()
 
+            # Update column and row counters
             column += 1
             if column >= columnCnt: 
                 column = 0
                 row += 1
     
     def create_detail_frame(self):
-
         self.details_frame = customtkinter.CTkFrame(self.root)
-        self.details_frame.pack_forget() 
+        self.details_frame.pack(expand=True)
+        self.details_frame.pack_forget()
 
-        self.details_frame.grid_rowconfigure(0, weight=1)
-        self.details_frame.grid_rowconfigure(1, weight=1)
-        self.details_frame.grid_rowconfigure(2, weight=1)
-        self.details_frame.grid_rowconfigure(3, weight=1)
-        self.details_frame.grid_rowconfigure(4, weight=1)
-        self.details_frame.grid_rowconfigure(5, weight=1)
-        self.details_frame.grid_columnconfigure(0, weight=1)
-        self.details_frame.grid_columnconfigure(1, weight=8)
-        self.details_frame.grid_columnconfigure(2, weight=30)
+        self.frame = customtkinter.CTkFrame(self.details_frame, width=800, height=600)
+        self.frame.place(relx=0.5, rely=0.5, anchor='center')
+
+    # self.details_frame.grid_rowconfigure(0, weight=1)
+    # self.details_frame.grid_rowconfigure(1, weight=1)
+    # self.details_frame.grid_rowconfigure(2, weight=1)
+    # self.details_frame.grid_rowconfigure(3, weight=1)
+    # self.details_frame.grid_rowconfigure(4, weight=1)
+    # self.details_frame.grid_rowconfigure(5, weight=1)
+    # self.details_frame.grid_columnconfigure(0, weight=1)
+    # self.details_frame.grid_columnconfigure(1, weight=8)
+    # self.details_frame.grid_columnconfigure(2, weight=30)
 
     def create_detail_widgets(self):
+        fields = [('학교명', 'name'), ('학과', 'department'), ('재직의무', 'obligation'), 
+                ('입학처 링크', 'link'), ('위치', 'location')]
+        for i, (label_text, entry_var) in enumerate(fields):
+            label = customtkinter.CTkLabel(self.frame, text=label_text)
+            label.grid(row=i, column=0, padx=10, pady=10, sticky='e') # padding added
+            entry = customtkinter.CTkTextbox(self.frame, height=20 if label_text == '학교명' or label_text == '위치' else 2, width=100) # Adjusted height and width
+            entry.grid(row=i, column=1, padx=10, pady=10, sticky='ew') # Adjusted 'sticky' parameter for horizontal expansion
+            setattr(self, 'details_' + entry_var + '_entry', entry)
 
-        # Create Name label and entry field
-        self.details_name_label = customtkinter.CTkLabel(self.details_frame, text="학교명")
-        self.details_name_label.grid(row=0, column=0, sticky='e')
-        self.details_name_entry = customtkinter.CTkTextbox(self.details_frame, height=2)
-        self.details_name_entry.grid(row=0, column=1, sticky='w')
-
-        # Create Department label and entry field
-        self.details_department_label = customtkinter.CTkLabel(self.details_frame, text="학과")
-        self.details_department_label.grid(row=1, column=0, sticky='e')
-        self.details_department_entry = customtkinter.CTkTextbox(self.details_frame, height=2)
-        self.details_department_entry.grid(row=1, column=1, sticky='w')
-
-        # Create Obligation label and entry field
-        self.details_obligation_label = customtkinter.CTkLabel(self.details_frame, text="재직의무")
-        self.details_obligation_label.grid(row=2, column=0, sticky='e')
-        self.details_obligation_entry = customtkinter.CTkTextbox(self.details_frame, height=2)
-        self.details_obligation_entry.grid(row=2, column=1, sticky='w')
-
-        # Create Link label and entry field
-        self.details_link_label = customtkinter.CTkLabel(self.details_frame, text="입학처 링크")
-        self.details_link_label.grid(row=3, column=0, sticky='e')
-        self.details_link_entry = customtkinter.CTkTextbox(self.details_frame, height=4)
-        self.details_link_entry.grid(row=3, column=1, sticky='w')
-
-        # Create Location label and entry field
-        self.details_location_label = customtkinter.CTkLabel(self.details_frame, text="위치")
-        self.details_location_label.grid(row=4, column=0, sticky='e')
-        self.details_location_entry = customtkinter.CTkTextbox(self.details_frame, height=20)
-        self.details_location_entry.grid(row=4, column=1, sticky='w')
-
+    
+    
     def create_details_button_frame(self):
-
-        self.details_button_frame = customtkinter.CTkFrame(self.details_frame)
-        self.details_button_frame.grid(row=5, column=0, columnspan=3)
+        self.details_button_frame = customtkinter.CTkFrame(self.frame)
+        self.details_button_frame.grid()
 
         self.back_button = customtkinter.CTkButton(self.details_button_frame, text="뒤로가기", command=self.back)
-        self.back_button.pack(side="left")
+        self.back_button.grid(row=0, column=0, padx=10, pady=10, sticky='w')
 
     def back(self):
         self.details_frame.pack_forget()
